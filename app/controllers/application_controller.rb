c2edce25a19db443d46d815cd9c161fa5795773a
camila-maia <camilinhamaia120050031@gmail.com>
2014-01-04 17:39:29 -0200

    Now it is possible to create and show products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
+require 'yaml'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  ###########################################
+  # SECURITY (security?)
+  ###########################################
+  logins = YAML::load_file(File.join(Dir.pwd, '/config', 'logins.yml'))
+  use Rack::Auth::Basic, "Protected Area" do |u, p|
+    logins[:users].has_key? u and logins[:users][u] == p
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Toxstock::Application.routes.draw do
-  resources :posts
+  resources :products
  
-  root 'welcome#index'
-
   # get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
