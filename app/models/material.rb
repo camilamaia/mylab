7e6c3ea43bac85f65184575d6250c1865c7ab3d8
camila-maia <camilinhamaia120050031@gmail.com>
2014-01-31 09:57:12 -0200

    I18n for simple_form

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,7 +1,7 @@
 class Material < ActiveRecord::Base
   extend Enumerize
 
-  enumerize :kind, in: [:permanente, :custeio]
+  enumerize :kind, in: [:durable, :not_durable]
   validates :name, presence: true
   validates :kind, presence: true
 end
