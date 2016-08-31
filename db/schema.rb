dea909af144ddd8f96e30d05a2d95335d30ff9bf
camilamaia <cmaiacd@gmail.com>
2016-08-31 15:31:32 -0300

    Create User and Stock models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140128014959) do
+ActiveRecord::Schema.define(version: 20160831181924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,20 @@ ActiveRecord::Schema.define(version: 20140128014959) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "kind"
+  end
+
+  create_table "stocks", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
