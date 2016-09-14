95473786a9bba0f15c9dac38da6ca4d507b17d14
camilamaia <cmaiacd@gmail.com>
2016-09-14 08:49:55 -0300

    Background changed

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock < ActiveRecord::Base
   belongs_to :responsible, :class_name => 'User', :foreign_key => 'user_id'
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :responsible, presence: true
 end
