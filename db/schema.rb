59d180144005009f56f1516d63e58ced521711fc
camilamaia <cmaiacd@gmail.com>
2016-09-22 11:13:14 -0300

    Added index to current_lab and removed laboratory_id column on Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920134418) do
+ActiveRecord::Schema.define(version: 20160922134635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,12 +61,11 @@ ActiveRecord::Schema.define(version: 20160920134418) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.integer  "laboratory_id"
     t.integer  "current_lab_id"
   end
 
+  add_index "users", ["current_lab_id"], name: "index_users_on_current_lab_id", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["laboratory_id"], name: "index_users_on_laboratory_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
