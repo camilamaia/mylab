c9d5709b26fa2f8b707ae1dad1bc676a2a45468f
camilamaia <cmaiacd@gmail.com>
2016-10-07 14:01:41 -0300

    Controllers refectory

--- app/controllers/item_utensils_controller.rb
+++ app/controllers/item_utensils_controller.rb
@@ -1,18 +1,19 @@
 class ItemUtensilsController < ApplicationController
   def new
     @item_utensil = ItemUtensil.new
-    @stock = Stock.find params[:stock]
+    @stock        = Stock.find params[:stock]
   end
 
   def show
     @item_utensil = ItemUtensil.find(params[:id])
-    @stock = Stock.find params[:stock]
+    @stock        = Stock.find params[:stock]
   end
 
   def create
-    @item_utensil = ItemUtensil.new(item_utensil_params)
+    @item_utensil             = ItemUtensil.new(item_utensil_params)
     @item_utensil.description = @item_utensil.friendly_description item_utensil_params
-    @stock = Stock.find params[:stock]
+    @stock                    = Stock.find params[:stock]
+    @item_utensil.stock       = @stock
 
     if @item_utensil.save
       redirect_to item_utensil_path(@item_utensil.id, stock: @stock.id)
@@ -23,12 +24,13 @@ class ItemUtensilsController < ApplicationController
 
   def edit
     @item_utensil = ItemUtensil.find(params[:id])
-    @stock = Stock.find params[:stock]
+    @stock        = Stock.find params[:stock]
   end
 
   def update
-    @item_utensil = ItemUtensil.find(params[:id])
-    @stock = Stock.find params[:stock]
+    @item_utensil             = ItemUtensil.find(params[:id])
+    @stock                    = Stock.find params[:stock]
+    @item_utensil.stock       = @stock
     @item_utensil.description = @item_utensil.friendly_description item_utensil_params
 
     if @item_utensil.update(item_utensil_params)
@@ -40,10 +42,10 @@ class ItemUtensilsController < ApplicationController
 
   def destroy
     @item_utensil = ItemUtensil.find(params[:id])
-    @stock = Stock.find params[:stock]
+    @stock        = Stock.find params[:stock]
     @item_utensil.destroy
 
-    redirect_to item_utensils_path
+    redirect_to items_path(:stock => @stock)
   end
 
   private
