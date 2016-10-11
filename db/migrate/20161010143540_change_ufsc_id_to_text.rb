2f6043acac87e5b38b7b8af779634ef68ff9de3c
camilamaia <cmaiacd@gmail.com>
2016-10-11 12:04:15 -0300

    Change id_ufsc type to string

--- db/migrate/20161010143540_change_ufsc_id_to_text.rb
+++ db/migrate/20161010143540_change_ufsc_id_to_text.rb
@@ -1,4 +1,4 @@
 class ChangeUfscIdToText < ActiveRecord::Migration
-  change_column :glasswares, :ufsc_id, :string, :limit => 1000
-  change_column :utensils, :ufsc_id, :string, :limit => 1000
+  change_column :glasswares, :ufsc_id, :string, :limit => 50
+  change_column :utensils, :ufsc_id, :string, :limit => 50
 end
